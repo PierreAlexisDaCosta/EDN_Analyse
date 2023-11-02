d6bdeeaed0f31b4e6e617f56267bd3ec78173f82
PierreAlexisDaCosta <pierrealexis.da.costa@gmail.com>
2023-11-02 01:52:27 +0100

    Initial commit

--- 4.Fonction qui calcule si un étudiant peut avoir à son poste Hors-contingent.R
+++ 4.Fonction qui calcule si un étudiant peut avoir à son poste Hors-contingent.R
@@ -63,3 +63,22 @@ poste_HC(Année = "2023",
          rang_étudiant = 5000, nombre_etudiant_classées = 10000,
          spécialité_désirée = "anesthésieréanimation", 
          subdivision_désirée = "CHU de Strasbourg")
+
+# github configuration ####
+library(usethis)
+library(gitcreds)
+usethis::edit_git_config() #Veryfy username and mail
+usethis::use_git()
+ usethis::create_github_token()
+# # This will take you to the appropriate page on the GitHub website,
+# # where you’ll give your token a name and copy it (don’t lose it because
+# # it will never appear again!).
+# Token_name : CER_Critère de rang
+# Token : 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
+gitcreds::gitcreds_set()
+
+usethis::use_git()
+usethis::use_github()
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+
+
